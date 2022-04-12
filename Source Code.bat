3d31b032ce09326113fdb2c3ae9a84de0e500c7b
Vengeance Studios <81897964+Techboi101@users.noreply.github.com>
2022-04-12 19:19:17 +1000

    Add files via upload

---
+++ Source Code.bat
@@ -0,0 +1,19 @@
+@echo off
+cls
+color 0a
+
+:app_prepare
+cls
+echo app loading
+echo please wait...
+timeout 5
+goto app
+
+:app
+cls
+echo Welcome to the Vengeance studios app!
+echo.
+echo.
+echo Website: vengeancestudios.godaddysites.com
+echo.
+echo Version: 1.0
